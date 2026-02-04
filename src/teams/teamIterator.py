f1dba82f5938613e006f95a37f15b5b6ec1cbcc0
Ryan Ramassamy <rayourama@hotmail.com>
2026-02-04 16:02:53 +0100

    FEAT-9-teamIterator

---
+++ src/teams/teamIterator.py
@@ -0,0 +1,12 @@
+class TeamIterator:
+    def __init__(self, team):
+        self.__team = team
+        self.__index = 0
+
+    def __next__(self):
+        if self.__index < len(self.__team):
+            result = self.__team[self.__index]
+            self.__index += 1
+            return result
+        else:
+            raise StopIteration()
