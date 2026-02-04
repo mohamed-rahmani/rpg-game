9fc8708ff90d05015e9a43143c4dd7f8cb695357
mohamed-rahmani <mohamed.rahmani.ma@gmail.com>
2026-02-04 15:25:48 +0100

    fix: retirer tous les parmaetres inutiles

--- src/character/wizard.py
+++ src/character/wizard.py
@@ -2,7 +2,7 @@ import random
 
 
 class Wizard:
-    def __init__(self, degat, chance, fuite, prix, type_unite):
+    def __init__(self):
         self.degat = random.randint(2, 4)
         self.chance = 20
         self.fuite = 10
