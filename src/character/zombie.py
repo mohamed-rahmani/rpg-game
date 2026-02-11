2be78dd3f986faa85a583e9faf1cce6a99492e1f
mohamed-rahmani <mohamed.rahmani.ma@gmail.com>
2026-02-11 16:10:01 +0100

    fix: trailing whitespace zombie

--- src/character/zombie.py
+++ src/character/zombie.py
@@ -4,5 +4,4 @@ import random
 class Zombie:
     def __init__(self):
         self.degat = random.randint(1, 2)
-        self.loot = random.randint(5, 10) / 10 
-        
+        self.loot = random.randint(5, 10) / 10
